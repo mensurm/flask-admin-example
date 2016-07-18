157b5c1e53e43bb129a559d35c5bd25be2d4e2d4
Mensur Mandzuka <=>
2016-07-18 15:20:14 +0200

    wrap application - inital Flask-admin view

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from flask_admin import Admin
 
 app = Flask(__name__)
+
+# Wrap application
+# name parameter is show on the default navebar
+# for the view bootstrap 2 or 3 can be selected
+admin = Admin(app, name='Flask admin example', template_mode='bootstrap3')
 
 @app.route('/')
 def hello():
