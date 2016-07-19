e7935a55a837589944d60a131137dc91aa42c2fd
Mensur Mandzuka <mensur.mandzuka@gmail.com>
2016-07-19 14:09:07 +0200

    static view added

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,27 @@
 from flask_admin.contrib.sqla import ModelView
+from flask.ext.admin import expose, BaseView
 from app.models import User
 from app import admin, db_session
 
-# create view for User class
+# 1. View created for CRUD operations on users table
+# Inherits ModelView class
 class UserView(ModelView):
     column_list = ('firstname', 'lastname')
 
     def __init__(self, session, **kwargs):
         super(UserView, self).__init__(User, session, **kwargs)
 
+# 2. Static view
+# Inherits BaseView class
+class StaticView(BaseView):
+
+    @expose('/', methods=('GET', 'POST'))
+    def static_view(self):
+        return self.render('admin/static_template.html')
+
+
 #bind UserView object to admin object
 admin.add_view(UserView(db_session))
+
+#register static view on admin object
+admin.add_view(StaticView(name="Static view"))
